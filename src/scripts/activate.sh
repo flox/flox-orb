15611f4445b5d1247ddd4dc2af7f79fd1e461e72
Rok Garbas <rok@flox.dev>
2024-09-10 23:40:03 +0200

    implement activate command

---
+++ src/scripts/activate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+COMMAND="${PARAM_COMMAND}"
+ENVIRONMENT="${PARAM_ENVIRONMENT}"
+
+if [ "$COMMAND" == "" ]; then
+  echo "command parameter is required."
+  exit 2
+fi
+
+ACTIVATE="flox activate"
+if [ "$ENVIRONMENT" != "" ]; then
+  ACTIVATE="$ACTIVATE --remote=$ENVIRONMENT"
+fi
+
+ACTIVATE="$ACTIVATE -- $COMMAND"
+
+eval $ACTIVATE
